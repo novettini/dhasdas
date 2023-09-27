c6b395b09f4820417de12d003ef311b5191641c3
Novettini <ovettini@gmail.com>
2023-09-27 15:03:04 -0300

    Agregado a mi branch.

---
+++ script/script.js
@@ -0,0 +1,22 @@
+aparecer = document.getElementById("formulario-oculto")
+seleccionado = document.formulario.motivo
+botonEnviar = document.getElementById("primer-submit")
+textareaFormulario = document.getElementById("agregados")
+// acá aparece o desaparece el formulario
+function mostrarOcultar(){
+    seleccionado.addEventListener("change", () => {
+    if(seleccionado.value == "agregar-cancha"){
+        aparecer.classList.remove("d-none")
+        botonEnviar.classList.add("d-none")
+        textareaFormulario.classList.add("d-none")
+ }
+else{
+   
+    aparecer.classList.add("d-none")
+    botonEnviar.classList.remove("d-none")
+    textareaFormulario.classList.remove("d-none")
+}
+})
+}
+// ---------
+window.onload = mostrarOcultar(); 
