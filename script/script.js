e795aa42687c01f421bc5fa784cd5ae8036cdab4
Novettini <ovettini@gmail.com>
2023-10-15 23:53:18 -0300

    Unido todo en el index principal

--- script/script.js
+++ script/script.js
@@ -1,11 +1,7 @@
-// formOculto = document.getElementsByClassName(form-oculto);
-// eleccion = document.getElementById('agregar');
 aparecer = document.getElementById("formulario-oculto")
-// select = document.getElementsByName("agregar-cancha")
 seleccionado = document.formulario.motivo
 botonEnviar = document.getElementById("primer-submit")
 textareaFormulario = document.getElementById("agregados")
-
 // acá aparece o desaparece el formulario
 function mostrarOcultar(){
     seleccionado.addEventListener("change", () => {
@@ -13,17 +9,14 @@ function mostrarOcultar(){
         aparecer.classList.remove("d-none")
         botonEnviar.classList.add("d-none")
         textareaFormulario.classList.add("d-none")
-    
-}
+ }
 else{
    
     aparecer.classList.add("d-none")
     botonEnviar.classList.remove("d-none")
     textareaFormulario.classList.remove("d-none")
-    
 }
 })
 }
 // ---------
-
 window.onload = mostrarOcultar(); 
